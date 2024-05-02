07ef54bcc2869c5da0f07b3bc0e9cc189442e7bb
VadimKorobec <_s_e_t__@ukr.net>
2024-05-02 17:32:57 +0300

    add some code

--- src/interface.ts
+++ src/interface.ts
@@ -3,11 +3,11 @@ interface AddFn {
   (n1: number, n2: number): number;
 }
 
-let add: AddFn;
+// let add: AddFn;
 
-add = (n1: number, n2: number) => {
-  return n1 + n2;
-};
+// add = (n1: number, n2: number) => {
+//   return n1 + n2;
+// };
 
 console.log(add(9, 9));
 
