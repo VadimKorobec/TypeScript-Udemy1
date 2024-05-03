3cece3f5316d625a4f0bbc6f34f3f693e3c1b948
VadimKorobec <_s_e_t__@ukr.net>
2024-05-03 14:41:01 +0300

    add some code

--- src/app.ts
+++ src/app.ts
@@ -1,144 +1,44 @@
-type Admin = {
-  name: string;
-  privileges: string[];
-};
+// const names: Array<string> = [];
 
-type Employee = {
-  name: string;
-  startDate: Date;
-};
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
 
-type ElevatedEmployee = Admin & Employee;
+// promise
+//   .then((data) => {
+//     data.map();
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
-const e1: ElevatedEmployee = {
-  name: "Cat",
-  privileges: ["create-server"],
-  startDate: new Date(),
-};
+// function merge<T, U>(objA: T, objB: U) {
+//   return Object.assign(objA, objB);
+// }
 
-type Combinable = string | number;
-type Numeric = number | boolean;
+// const mergeObj = merge({ name: "Cat" }, { age: 30 });
 
-type Universal = Combinable & Numeric;
-
-function add(a: number, b: number): number;
-function add(a: string, b: string): string;
-function add(a: Combinable, b: Combinable) {
-  if (typeof a === "string" || typeof b === "string") {
-    return a.toString() + b.toString();
-  }
-  return a + b;
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return { ...objA, ...objB };
 }
 
-const result = add("Cat", "cat");
-result.split(" ");
+const mergeObj = merge({ name: "Cat", hobbies: ["Sports"] }, { age: 30 });
+console.log(mergeObj);
 
-const fetcUserDate = {
-  id: "1",
-  name: "Cat",
-  job: { title: "Just cat", description: "I am a cat" },
-};
+interface Lenghty {
+  length: number;
+}
 
-console.log(fetcUserDate?.job?.title);
+function countAndDescribe<T extends Lenghty>(element: T) {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = `Got 1 element.`;
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements`;
+  }
+  return [element, descriptionText];
+}
 
-const userInput = 0;
-
-const storeData = userInput ?? "DEFAULT";
-
-console.log(storeData);
-
-// console.log(add(5, 7));
-
-// type UnknownEmployee = Employee | Admin;
-
-// function printEmployeeInformation(emp: UnknownEmployee) {
-//   console.log("Name: " + emp.name);
-//   if ("privileges" in emp) {
-//     console.log("Privileges: " + emp.privileges);
-//   }
-
-//   if ("startDate" in emp) {
-//     console.log("StartDate: " + emp.startDate);
-//   }
-// }
-
-// printEmployeeInformation(e1);
-
-// class Car {
-//   drive(): void {
-//     console.log("Driving...");
-//   }
-// }
-
-// class Truck {
-//   drive(): void {
-//     console.log("Driving a truck...");
-//   }
-
-//   loadCargo(amount: number): void {
-//     console.log("Loading cargo ..." + amount);
-//   }
-// }
-
-// type Vehicle = Car | Truck;
-
-// const v1 = new Car();
-// const v2 = new Truck();
-
-// function useVehicle(vehicle: Vehicle) {
-//   vehicle.drive();
-
-//   if (vehicle instanceof Truck) {
-//     vehicle.loadCargo(1000);
-//   }
-// }
-
-// useVehicle(v1);
-// useVehicle(v2);
-
-// interface Bird {
-//   type: "bird";
-//   flyingSpeed: number;
-// }
-
-// interface Horse {
-//   type: "horse";
-//   runningSpeed: number;
-// }
-
-// type Animal = Bird | Horse;
-
-// function moveAnimal(animal: Animal) {
-//   let speed;
-//   switch (animal.type) {
-//     case "bird":
-//       speed = animal.flyingSpeed;
-//       break;
-//     case "horse":
-//       speed = animal.runningSpeed;
-//       break;
-//   }
-
-//   console.log("Moving at speed: " + speed);
-// }
-
-// moveAnimal({ type: "bird", flyingSpeed: 20 });
-// moveAnimal({ type: "horse", runningSpeed: 40 });
-
-// // const input = <HTMLInputElement>document.getElementById("user-input")!;
-// const input = (<HTMLInputElement>(
-//   document.getElementById("user-input")!
-// )) as HTMLInputElement;
-
-// input.value = "Hi there";
-
-// interface ErrorContainer {
-//   [prop: string]: string;
-// }
-
-// const errorBag: ErrorContainer = {
-//   email: "Not a valid email",
-//   userName: "Must start with a capital character!",
-// };
-
-// console.log(errorBag);
+console.log(countAndDescribe("Cat"));
